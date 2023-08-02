bc9391c1ef24b4ba8f80e31b3a92e0b236b692c3
Eduardo Marra <eduardomarra@gmail.com>
2023-08-02 18:16:36 -0300

    corrected detail route

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,7 +8,7 @@ export function Router() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/details/:id" element={<Details />} />
+        <Route path="/post/:id" element={<Details />} />
       </Route>
     </Routes>
   )
