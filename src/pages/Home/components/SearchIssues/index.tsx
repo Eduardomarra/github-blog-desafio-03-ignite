a4ccc24dbc9ea1e4d49cfd81fa36278b861ec4aa
Eduardo Marra <eduardomarra@gmail.com>
2023-08-02 18:15:08 -0300

    added post search

--- src/pages/Home/components/SearchIssues/index.tsx
+++ src/pages/Home/components/SearchIssues/index.tsx
@@ -1,14 +1,40 @@
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
 import { SearchIssuesContainer, SearchTitle } from './styles'
+import { useContext } from 'react'
+import { PostContext } from '../../../../contexts/PostContext'
+
+const searchFormSchema = zod.object({
+  query: zod.string(),
+})
+
+type SearchFormInput = zod.infer<typeof searchFormSchema>
 
 export function SearchIssues() {
+  const { posts, getPosts } = useContext(PostContext)
+
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchIssues(data: SearchFormInput) {
+    await getPosts(data.query)
+  }
+
   return (
     <SearchIssuesContainer>
       <SearchTitle>
         <h3>Publicações</h3>
-        <span>3 publicações</span>
+        <span>{posts.length} publicações</span>
       </SearchTitle>
-      <form>
-        <input type="text" placeholder="Buscar conteúdo" />
+      <form onSubmit={handleSubmit(handleSearchIssues)}>
+        <input
+          type="text"
+          placeholder="Buscar conteúdo"
+          {...register('query')}
+        />
       </form>
     </SearchIssuesContainer>
   )
